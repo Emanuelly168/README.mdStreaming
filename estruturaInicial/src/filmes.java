e4768df0708385904ef6ac02b11de40097d109d7
GuiEmanuell <guilhermeemanuel2005@gmail.com>
2025-03-26 21:20:41 -0300

    Add files via upload

---
+++ estruturaInicial/src/filmes.java
@@ -0,0 +1,21 @@
+public class filmes {
+    String nome;
+    String genero;
+    int duracao;
+    int ano;
+    int faixaEtaria;
+    int avalicao;
+
+    void exibeFilme(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Genêro: " + genero);
+        System.out.println("Duração (em minutos): " + duracao);
+        System.out.println("Ano de lançamento: " + ano);
+        System.out.println("Classisficação Indicada: " + faixaEtaria);
+        System.out.println("Avaliação: " + avalicao);
+    }
+
+
+
+
+}
---
+++ estruturaInicial/src/principalMoovix.java
@@ -0,0 +1,24 @@
+public class principalMoovix {
+    public static void main(String[] args) throws Exception {
+
+        filmes card = new filmes();
+        card.nome = "Crespuculo";
+        card.genero = "Drama, Fantasia, Romance";
+        card.duracao = 130;
+        card.ano = 2008;
+        card.faixaEtaria = 12;
+        card.avalicao = 5;
+
+        card.exibeFilme();
+
+
+
+
+
+
+
+
+
+    }
+
+}
